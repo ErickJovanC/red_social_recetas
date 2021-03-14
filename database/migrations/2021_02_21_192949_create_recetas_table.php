3ad8e1bf6ea669acd688442dad4bbb5cb82c8dd9
Erick Jovan <erickjovan106@gmail.com>
2021-03-14 17:55:31 -0600

    Se agrego la migración completa de 'recetas' y el inicio de 'categoría'

--- database/migrations/2021_02_21_192949_create_recetas_table.php
+++ database/migrations/2021_02_21_192949_create_recetas_table.php
@@ -13,10 +13,26 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+        Schema::create('categoria_receta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->timestamps();
+        });
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
 
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            $table->foreignId('user_id')-> 
+                references('id')-> 
+                on('users')-> 
+                comment('Usuario que crea la receta');
+            $table->foreignId('categoria_id')-> 
+                references('id')-> 
+                on('categoria_receta')->
+                comment('Categoria de la receta');
 
             $table->timestamps();
         });
@@ -29,6 +45,7 @@ class CreateRecetasTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('categoria_receta');
         Schema::dropIfExists('recetas');
     }
 }
