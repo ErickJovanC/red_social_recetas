8f655a7320fb66d7529ee15e98b34c416b439401
Erick Jovan <erickjovan106@gmail.com>
2021-03-14 22:52:23 -0600

    Se agrego el editor de trix al formulario create, y se realizo la validación

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
+
+@section('styles')
+{{-- <link rel="stylesheet" href="{{ asset('css/trix.css') }}"> --}}
+{{-- <link rel="stylesheet" href="{{ asset('css/trix.css') }}" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" /> --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" />
+@endsection
+
 @section('botones')
     <a href="{{ route('recetas.index') }}" class="btn btn-primary mr-2 text-white">Volver</a>
 @endsection
@@ -50,10 +57,53 @@
                     @enderror
                 </div>
 
+                <div class="form-group mt-3">
+                    <label for="preparacion">Preparación</label>
+                    <input 
+                        id="preparacion" 
+                        type="hidden" 
+                        name="preparacion" 
+                        value="{{ old('preparacion') }}"
+                    />
+                    <trix-editor 
+                        input="preparacion" class="form-control @error('preparacion') is-invalid @enderror">
+                    </trix-editor>
+
+                    @error('preparacion')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group mt-3">
+                    <label for="ingredientes">Ingredientes</label>
+                    <input 
+                        id="ingredientes" 
+                        type="hidden" 
+                        name="ingredientes" 
+                        value="{{ old('ingredientes') }}"
+                    />
+                    <trix-editor 
+                        input="ingredientes" class="form-control @error('ingredientes') is-invalid @enderror">
+                    </trix-editor>
+                    @error('ingredientes')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{$message}}</strong>
+                        </span>
+                    @enderror
+                </div>
+
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Agregar Receta">
                 </div>
             </form>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    {{-- <script src="{{ asset('js/trix.js') }}"></script> --}}
+    {{-- <script src="{{ asset('js/trix.js') }}" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js" integrity="sha512-/1nVu72YEESEbcmhE/EvjH/RxTg62EKvYWLG3NdeZibTCuEtW5M4z3aypcvsoZw03FAopi94y04GhuqRU9p+CQ==" crossorigin="anonymous"></script>
 @endsection
